e92bb1775bc57ada209d5f498442a9b5e0af70d2
a01245946 <72631858+a01245946@users.noreply.github.com>
2020-10-09 15:52:57 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let Fahrenheit = 0
+let _23 = 0
+let _15 = 0
+let Temperatura = input.lightLevel()
+let Grados = input.temperature()
+if (Fahrenheit < (_15 && _23)) {
+    basic.showString("“Toma tus mejores esquís y diviértete”")
+} else {
+    basic.showString("“Semáforo verde”")
+}
